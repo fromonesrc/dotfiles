901cb8d53451241ac1cdd9a58ce4905cf4a7129c
Adam Ochonicki <adam@fromonesrc.com>
2013-05-08 19:18:07 -0400

    A few aliases and ignores

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,16 @@
-alias reload!='. ~/.zshrc'
+alias reload!=". ~/.zshrc"
 
 alias b=bundle
-alias bi='b install --path vendor/bundle'
-alias bil='bi --local'
-alias bu='b update'
-alias be='b exec'
+alias bi="b install --path vendor/bundle"
+alias bil="bi --local"
+alias bu="b update"
+alias be="b exec"
 alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
 
 alias ll='ls -Falt'
-
 alias boxen='boxen --stealth'
-
 alias proc='sudo execsnoop -a'
-
 alias hosts='sudo vim /etc/hosts'
-
 alias instances='ec2-describe-instances'
+alias ll="ls -Falt"
+alias v="vim"
