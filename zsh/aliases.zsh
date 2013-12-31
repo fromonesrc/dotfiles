83beff86ea434364244eafad905001dfe03d39d9
Adam Ochonicki <adam@fromonesrc.com>
2013-12-31 11:12:19 -0500

    Vagrant aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,14 +7,17 @@ alias bu="b update"
 alias be="b exec"
 alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
 
-alias ll='ls -Falt'
-alias boxen='boxen --stealth'
-alias proc='sudo execsnoop -a'
-alias hosts='sudo vim /etc/hosts'
-alias instances='ec2-describe-instances'
+alias ll="ls -Falt"
+alias boxen="boxen --stealth"
+alias proc="sudo execsnoop -a"
+alias hosts="sudo vim /etc/hosts"
+alias instances="ec2-describe-instances"
 alias ll="ls -Falt"
 alias v="vim"
-alias clone='git clone'
-alias rmdir='sudo rm -r'
+alias clone="git clone"
+alias rmdir="sudo rm -r"
 
-alias gnuke='git reset --hard && git clean -df'
+alias gnuke="git reset --hard && git clean -df"
+
+alias vu="vagrant up"
+alias vp="vagrant provision"
