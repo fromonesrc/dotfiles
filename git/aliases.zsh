40bfb7b33c36dd74fd30084ea21d3f26447539da
Adam Ochonicki <adam@fromonesrc.com>
2015-08-21 15:51:44 -0400

    Git commit verbose

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,8 @@ fi
 
 alias clone="git clone"
 alias gb="git branch"
-alias gc="git commit"
-alias gca="git commit -a"
+alias gc="git commit --verbose"
+alias gca="gc -a"
 alias gco="git checkout"
 alias gd="git diff"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
